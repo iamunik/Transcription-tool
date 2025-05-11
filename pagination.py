8bdeba6e285834cf16cb3a660ea521257b86011e
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-05-12 00:04:51 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+ffmpeg_dir = os.path.join(os.path.dirname(__file__), "bin")
+os.environ["PATH"] = ffmpeg_dir + os.pathsep + os.environ.get("PATH", "")
 
 # Page Navigation
 homepage = st.Page("webpages/homepage.py", title="About Project", icon=":material/info:")
