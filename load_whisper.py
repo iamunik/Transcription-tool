e085f4711863fce4dc60735cbad948f9b9a4209f
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-05-12 12:34:23 +0100

    Utilized streamlit cache_resources to remove loading the model evrytime a page refreshes

--- load_whisper.py
+++ load_whisper.py
@@ -1,16 +1,17 @@
 # load_and_save_model.py
 import whisper
+import streamlit as st
 import os
 
 
+@st.cache_resource(show_spinner="Loading Whisper model...")
 def load_and_save_model(model_name="base", model_dir="whisper_models"):
     """Download and save Whisper model to a folder."""
-    # Ensure the directory exists
+    # Ensures the directory already exist
     os.makedirs(model_dir, exist_ok=True)
 
     # Load the model and cache it in the specified directory
     model = whisper.load_model(model_name, download_root=model_dir)
-    # print(f"Model loaded from {model_dir}")
     return model
 
 
