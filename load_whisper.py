7e15f65938e0953cbab6c368a0e5615b6299e1c9
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-06-08 18:33:16 +0100

    Update load_whisper.py

--- load_whisper.py
+++ load_whisper.py
@@ -16,7 +16,7 @@ import os
 
 
 @st.cache_resource(show_spinner="Loading Faster-Whisper model...")
-def load_and_save_model(model_name="tiny", model_dir="whisper_models"):
+def load_and_save_model(model_name="base", model_dir="whisper_models"):
     os.makedirs(model_dir, exist_ok=True)
     # device="cpu" or "cuda", compute_type="int8" or "float16" for GPU
     whisper_model = WhisperModel(model_name, download_root=model_dir, device="cpu", compute_type="int8")
