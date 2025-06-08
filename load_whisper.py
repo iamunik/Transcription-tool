3e27acaab1e93e36ed19d9f14014fc89d25b5aca
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-06-08 17:15:37 +0100

    Update load_whisper.py

--- load_whisper.py
+++ load_whisper.py
@@ -1,20 +1,27 @@
-# load_and_save_model.py
-import whisper
+# import whisper
+# import streamlit as st
+# import os
+#
+# @st.cache_resource(show_spinner="Loading Whisper model...")
+# def load_and_save_model(model_name="tiny", model_dir="whisper_models"):
+#     os.makedirs(model_dir, exist_ok=True)
+#     model = whisper.load_model(model_name, download_root=model_dir)
+#     return model
+#
+# if __name__ == "__main__":
+#     whispers = load_and_save_model()
+from faster_whisper import WhisperModel
 import streamlit as st
 import os
 
 
-@st.cache_resource(show_spinner="Loading Whisper model...")
+@st.cache_resource(show_spinner="Loading Faster-Whisper model...")
 def load_and_save_model(model_name="tiny", model_dir="whisper_models"):
-    """Download and save Whisper model to a folder."""
-    # Ensures the directory already exist
     os.makedirs(model_dir, exist_ok=True)
-
-    # Load the model and cache it in the specified directory
-    model = whisper.load_model(model_name, download_root=model_dir)
-    return model
+    # device="cpu" or "cuda", compute_type="int8" or "float16" for GPU
+    whisper_model = WhisperModel(model_name, download_root=model_dir, device="cpu", compute_type="int8")
+    return whisper_model
 
 
 if __name__ == "__main__":
-    whispers = load_and_save_model()  # This will download the model if not already cached
-    # print("Model loaded and saved to disk!")
+    model = load_and_save_model()
