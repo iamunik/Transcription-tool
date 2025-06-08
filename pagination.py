69c71ebef58910ad34962e0845bf9b3c03793e1e
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-06-08 18:33:44 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -4,10 +4,8 @@ import os
 ffmpeg_dir = os.path.join(os.path.dirname(__file__), "bin")
 os.environ["PATH"] = ffmpeg_dir + os.pathsep + os.environ.get("PATH", "")
 
-# Page Navigation
 homepage = st.Page("webpages/homepage.py", title="About Project", icon=":material/info:")
 delivery_page = st.Page("webpages/app.py", title="Transcription", icon=":material/local_hospital:")
 
 pg = st.navigation([homepage, delivery_page,])
-
 pg.run()
